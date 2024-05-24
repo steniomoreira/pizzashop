3a45ed5746f37ada71bad2624f96b9f5912fc31a
steniomoreira <steniomoreiradev@gmail.com>
2024-05-23 22:39:18 -0300

    feat: add chart per period

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,63 @@
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '10/05', revenue: 1200 },
+  { date: '11/05', revenue: 800 },
+  { date: '12/05', revenue: 900 },
+  { date: '13/05', revenue: 400 },
+  { date: '14/05', revenue: 2300 },
+  { date: '15/05', revenue: 800 },
+  { date: '16/05', revenue: 640 },
+]
+
+export function RevanueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet[500]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
